55fa400694cc524d3efe7675561fdc3f43de95b6
badair <badair@harding.edu>
2016-04-06 22:41:35 -0500

    documentation

--- example/intro.cpp
+++ example/intro.cpp
@@ -9,17 +9,6 @@ Distributed under the Boost Software License, Version 1.0.
 #include <tuple>
 #include <callable_traits/callable_traits.hpp>
 
-// Most of this example uses a function object. Unless otherwise noted, all
-// features shown in this example can be used for any "callable" types:
-// lambdas, generic lambdas, function pointers, function references,
-// function types, abominable function types, member function pointers, and
-// member data pointers. Ambiguous callables (e.g. function objects with
-// templated/overloaded operator()) are not addressed in this example, but
-// are recognized and handled by CallableTraits.
-
-// Note: For more information about abominable function types, see Alisdair Meredith's
-// proposal at http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/p0172r0.html
-
 struct foo {
     void operator()(int, int&&, const int&, void* = nullptr) const {}
 };
@@ -39,55 +28,55 @@ int main() {
     using expected_args = std::tuple<int, int&&, const int&, void*>;
     static_assert(std::is_same<args, expected_args>::value, "");
 
-    //callable_traits::result_of is a bit friendlier than std::result_of 
+    //callable_traits::result_of is a bit friendlier than std::result_of
     using return_type = ct::result_of<foo>;
     static_assert(std::is_same<return_type, void>::value, "");
 
-    // callable_traits::function_type yields a plain function type
+    //has_void_return is a quicker way to perform the check above
+    static_assert(ct::has_void_return(foo{}), "");
+
+    // callable_traits::function_type decays a callable type to
+    // a plain function type, which is structured in terms of INVOKE
     using function_type = ct::function_type<foo>;
     using expected_function_type = void(int, int&&, const int&, void*);
     static_assert(std::is_same<function_type, expected_function_type>::value, "");
 
-    // when trait information can be conveyed in an integral_constant,
-    // callable_traits uses constexpr functions instead of template aliases.
-    // Note: min_arity and max_arity only differ logically from arity when
-    // the argument is a function object.
+    // when trait information can be conveyed in an std::integral_constant,
+    // callable_traits opts for constexpr functions instead of template aliases.
+    // This is done to encourage value semantics, and to simplify usage inside
+    // of forwarding functions.
     static_assert(ct::arity(foo{}) == 4, "");
     static_assert(ct::max_arity(foo{}) == 4, "");
     static_assert(ct::min_arity(foo{}) == 3, "");
 
     // CallableTraits provides constexpr functions so that the user doesn't
     // need to worry about reference collapsing or decltype when dealing with
-    // universal references to callables. Still, you don't need an instance,
+    // universal references to callables. Still, you don't NEED an instance,
     // because CallableTraits provides non-deduced function templates for
-    // all constexpr functions besides can_invoke and bind, which
-    // model std::invoke and std::bind, respectively (more on these below).
-    // Here's an example of the non-deduced functions, which take an explicit
-    // type argument. We'll ignore these for the rest of the example.
+    // all constexpr functions (except for can_invoke/can_invoke_constexpr and bind,
+    // which model std::invoke and std::bind, respectively -- more on these below).
+    // Here's an example of the non-deduced version of arity, which take an
+    // explicit type argument. We'll ignore these non-deduced overloads for the
+    // rest of this example.
     static_assert(ct::arity<foo>() == 4, "");
-    static_assert(ct::max_arity<foo>() == 4, "");
-    static_assert(ct::min_arity<foo>() == 3, "");
 
-    // C-style varargs (ellipses in a signature) can be detected.
+    // C-style variadics detection (ellipses in a signature)
     static_assert(!ct::has_varargs(foo{}), "");
 
-    // callable_traits::can_invoke allows us to preview whether
-    // std::invoke will compile with the given arguments. Keep
-    // in mind that failing cases must be SFINAE-friendly (i.e.
-    // any failing static_asserts can still be tripped). Note: The
-    // same sfinae restrictions apply to min_arity and max_arity
-    // for function objects.
     int i = 0;
+
+    // callable_traits::can_invoke allows us to preview whether std::invoke will
+    // compile with the given arguments.
     static_assert(ct::can_invoke(foo{}, 0, 0, i), "");
     // no error:     std::invoke(foo{}, 0, 0, i);
 
     static_assert(!ct::can_invoke(foo{}, nullptr), "");
     // error:         std::invoke(foo{}, nullptr);
 
-    // For function objects, the following checks are determined by the 
+    // For function objects, the following checks are determined by the
     // qualifiers on operator(), rather than the category of the passed value.
     // For member function pointers and abominable function types, the
-    // qualifier on the function type are used.
+    // qualifiers on the function type are used.
     static_assert(ct::is_const_qualified(foo{}), "");
     static_assert(!ct::is_volatile_qualified(foo{}), "");
     static_assert(!ct::is_reference_qualified(foo{}), "");
@@ -102,43 +91,30 @@ int main() {
 
     using pmf = decltype(&foo::operator());
 
-    {
-        // So that you don't have to scroll back up to see, here's the type of pmf:
-        using expected_pmf = void (foo::*)(int, int&&, const int&, void*) const;
-        static_assert(std::is_same<pmf, expected_pmf>::value, "");
-    }
+    // So that you don't have to scroll back up to see, here's the type of pmf:
+    using with_const = void (foo::*)(int, int&&, const int&, void*) const;
+    static_assert(std::is_same<pmf, with_const>::value, "");
 
-    {
-        // Let's remove the const qualifier:
-        using mutable_pmf = ct::remove_const_qualifier<pmf>;
-        using expected_pmf = void (foo::*)(int, int&&, const int&, void*) /*no const!*/;
-        static_assert(std::is_same<mutable_pmf, expected_pmf>::value, "");
-    }
+    // Let's remove the const qualifier:
+    using mutable_pmf = ct::remove_const_qualifier<pmf>;
+    using without_const = void (foo::*)(int, int&&, const int&, void*) /*no const!*/;
+    static_assert(std::is_same<mutable_pmf, without_const>::value, "");
 
-    {
-        // Now let's add an rvalue qualifier (&&):
-        using rvalue_pmf = ct::add_rvalue_qualifier<pmf>;
-        using expected_pmf = void (foo::*)(int, int&&, const int&, void*) const &&;
-        static_assert(std::is_same<rvalue_pmf, expected_pmf>::value, "");
-    }
+    // Now let's add an rvalue qualifier (&&):
+    using rvalue_pmf = ct::add_rvalue_qualifier<pmf>;
+    using with_rvalue = void (foo::*)(int, int&&, const int&, void*) const &&;
+    static_assert(std::is_same<rvalue_pmf, with_rvalue>::value, "");
 
     // You get the picture. CallableTraits lets you add and remove all PMF
     // qualifiers (const, volatile, &, &&, and any combination thereof).
-    // These type operations can be performed on abominable function types as well.
+    // These type operations can generally be performed on abominable function
+    // types as well.
 
+    // is_constexpr would return std::true_type if foo's operator() were constexpr.
+    static_assert(!ct::is_constexpr(foo{}), "");
 
-    // Somehow, somewhere, there was a sad programmer who wanted to
-    // manipulate c-style varargs in a function signature. There's
-    // really no way to do this universally without doing all the work
-    // that CallableTraits already does, so CallableTraits includes in
-    // an add/remove feature for this, too.
-    {
-
-        using varargs_pmf = ct::add_varargs<pmf>;
-        using expected_pmf = void (foo::*)(int, int&&, const int&, void*, ...) const;
-        static_assert(std::is_same<varargs_pmf, expected_pmf>::value, "");
-
-        // note: MSVC likely requires __cdecl for a varargs PMF on your
-        // machine, at least if you intend to do anything useful with it.
-    }
+    // to check constexprness of a function or member function, you must use an
+    // std::integral_constant, like this:
+    using pmf_constant = std::integral_constant<pmf, &foo::operator()>;
+    static_assert(!ct::is_constexpr(pmf_constant{}), "");
 }
