5fd992e0aa8f61bf43bc1692bc4d1d7535924439
badair <badair@harding.edu>
2016-06-20 19:37:07 -0500

    [documentation] remove_member_reference

--- include/callable_traits/remove_member_reference.hpp
+++ include/callable_traits/remove_member_reference.hpp
@@ -12,6 +12,14 @@ Distributed under the Boost Software License, Version 1.0.
 
 #include <callable_traits/detail/core.hpp>
 
+//[ remove_member_reference_hpp
+/*`
+[section:ref_remove_member_reference remove_member_reference]
+[heading Header]
+``#include<callable_traits/remove_member_reference.hpp>``
+[heading Definition]
+*/
+
 namespace callable_traits {
 
     template<typename T>
@@ -27,5 +35,35 @@ namespace callable_traits {
         using type = remove_member_reference_t<T>;
     };
 }
+
+/*`
+[heading Constraints]
+* `T` must be a function type or a member function pointer type
+
+[heading Behavior]
+* A substitution failure occuers if the constraints are violated.
+* Removes member `&` or `&&` qualifiers from `T`, if present.
+
+[heading Input/Output Examples]
+[table
+    [[`T`]                              [`remove_member_const_t<T>`]]
+    [[`int() &`]                        [`int()`]]
+    [[`int(foo::*)() &`]                [`int(foo::*)()`]]
+    [[`int(foo::*)() const &`]          [`int(foo::*)() const`]]
+    [[`int(foo::*)() const &&`]         [`int(foo::*)() const`]]
+    [[`int(foo::*)()`]                  [`int(foo::*)()`]]
+    [[`int`]                            [(substitution failure)]]
+    [[`int (&)()`]                      [(substitution failure)]]
+    [[`int (*)()`]                      [(substitution failure)]]
+    [[`int foo::*`]                     [(substitution failure)]]
+    [[`int (foo::* const)()`]           [(substitution failure)]]
+]
+
+[heading Example Program]
+[import ../example/remove_member_reference.cpp]
+[remove_member_reference]
+[endsect]
+*/
+//]
 
 #endif
